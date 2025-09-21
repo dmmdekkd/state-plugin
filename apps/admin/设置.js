805fedf16fdb6c513fa02b4d8f86ac1c6107e985
🌌 <Time.Rain.Star.Sky@Gmail.com>
2025-09-21 18:27:05 +0800

    feat

--- apps/admin/设置.js
+++ apps/admin/设置.js
@@ -19,18 +19,20 @@ const indexCfgTypeMap = {
   }
 }
 
-// 支持 #椰奶设置状态前缀 新前缀
-const indexCfgReg = new RegExp(`^#状态设置(${Object.keys(indexCfgTypeMap).join("|")}|前缀)(开启|关闭|(\\d+)秒?|\\s.+)?$`)
+// 统一支持 #sp设置 / #state设置 
+const indexCfgReg = new RegExp(
+  `^#?(?:state|sp)设置(${Object.keys(indexCfgTypeMap).join("|")}|前缀)(开启|关闭|(\\d+)秒?|\\s.+)?$`
+)
 
 export class Admin_Index extends plugin {
   constructor() {
     super({
-      name: "状态配置-index",
+      name: "状态配置",
       event: "message",
       priority: 100,
       rule: [
         { reg: indexCfgReg, fnc: "indexSet" },
-        { reg: "^#状态设置$", fnc: "sendImg" }
+        { reg: "^#?(?:state|sp)设置$", fnc: "sendImg" }
       ]
     })
   }
@@ -45,7 +47,9 @@ export class Admin_Index extends plugin {
 
     if (rawkey === "前缀") {
       // 修改状态前缀，重启生效
-      if (!value) return e.reply("请提供新的状态前缀，例如：#椰奶设置状态前缀 新前缀")
+      if (!value) {
+        return e.reply("请提供新的状态前缀，例如：#sp设置前缀 新前缀")
+      }
       Config.modify("state", "keyword", value)
       // 修改后返回配置图片
       return this.sendImg(e)
@@ -53,7 +57,7 @@ export class Admin_Index extends plugin {
 
     // 普通配置处理
     let _key = indexCfgTypeMap[rawkey]
-    let [ file, ...key ] = _key.toString().split(".")
+    let [file, ...key] = _key.toString().split(".")
     key = key.join(".")
 
     if (typeof _key === "object") {
@@ -79,25 +83,25 @@ export class Admin_Index extends plugin {
 
   getIndexSetData() {
     return {
-      label: "#椰奶设置",
+      label: "#状态设置",
       list: {
         系统设置: [
           {
-            key: "椰奶作为默认状态",
+            key: "作为默认状态",
             value: getStatus(Config.state.defaultState),
-            hint: "#椰奶设置状态 + 开启/关闭",
+            hint: "#sp设置状态 开启/关闭",
             desc: "开启后将使用椰奶版状态作为yunzai的默认状态"
           },
           {
             key: "渲染精度",
             value: getStatus(Config.other.renderScale),
-            hint: "#椰奶设置渲染精度100",
+            hint: "#sp设置渲染精度100",
             desc: "可选值50~200，建议100。设置高精度会提高图片的精细度，但因图片较大可能会影响渲染与发送速度"
           },
           {
             key: "状态前缀",
-            value:  getStatus(Config.state.keyword) || "椰奶",
-            hint: `椰奶设置前缀 新前缀`,
+            value: getStatus(Config.state.keyword) || "椰奶",
+            hint: `#sp设置前缀 新前缀`,
             desc: "修改状态命令触发前缀，修改后需要重启插件或机器人才能生效"
           }
         ]
